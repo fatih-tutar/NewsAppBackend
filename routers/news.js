bb9aaa43fac13b80b2b0c802658eed0ac13aeae9
fatih <fatih.tutar@dijitalvarliklar.com.tr>
2022-12-12 15:45:17 +0300

    Node JS image resizebefore saving

--- routers/news.js
+++ routers/news.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const sharp = require('sharp');
+const multer = require('multer');
+const fs = require('fs');
+const News = require('../news/news');
+const imageProcess = require('../util/imageProcess');
 
-router.post('/create', (req, res) => {
-    res.send('submit successful')
+const storage = multer.memoryStorage();
+const uploads = multer({storage});
+
+router.post('/create', uploads.single('thumbnail'), async (req, res) => {
+    const news = new News();
+    const id = news.createId();
+
+    const imageName = await imageProcess(req, id);
+
+    news.create(req.body, id, imageName); // http://localhost:3000/image-name
+    res.send('submit successful');
 })
 
 module.exports = router;
